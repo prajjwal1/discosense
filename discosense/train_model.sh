70c15c37f6ff93d5d9d8a52c81d08fdb2f3cc573
prajjwal1 <prajjwalin@protonmail.com>
2021-05-28 02:56:44 +0900

    added training script

--- discosense/train_model.sh
+++ discosense/train_model.sh
@@ -1,8 +1,1 @@
-export CONTEXT_COL=sentence2
-export TO_PREDICT_COL=sentence1
-
-CUDA_VISIBLE_DEVICES=0,1 deepspeed --num_gpus=2 ./run_clm_discovery.py --model_name_or_path ctrl --do_train --do_eval \
-        --deepspeed ds_config.json --dataset_name discovery --dataset_config_name discovery --per_device_train_batch_size 12 \
-        --per_device_eval_batch_size 10 --output_dir ~/experiments/ctrl_discovery_flipped_6 \
-        --preprocessing_num_workers 4 --evaluation_strategy no --tokenizer_name ctrl --gradient_accumulation_steps 12 \
-        --context_col $CONTEXT_COL --to_predict_next_col $TO_PREDICT_COL
+python3 run_clm_discovery.py --model_name_or_path ctrl --do_train --do_eval --per_device_train_batch_size 24 --per_device_eval_batch_size 42 --output_dir ~/apex/experiment/ctrl_discovery_flipped_6  --preprocessing_num_workers 4 --evaluation_strategy no --tokenizer_name ctrl --fp16 --dataset_name discovery --dataset_config_name discovery --context_col sentence2 --to_predict_next_col sentence1 --save_total_limit 1 --save_steps 20000

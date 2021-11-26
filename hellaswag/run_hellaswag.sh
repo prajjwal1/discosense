04e3aa5a9fcea6de2b6fd520adf5e13669eb2337
prajjwal1 <prajjwalin@protonmail.com>
2021-11-26 14:56:44 -0600

    after acl

--- hellaswag/run_hellaswag.sh
+++ hellaswag/run_hellaswag.sh
@@ -1,26 +1,13 @@
-export MODEL_PATH='bert-base-uncased'
-export OUTPUT_PATH='/shared/mlrdir3/disk1/pxb190028/models/hellaswag/bert-base-uncased'
-export BS=32
+export BS=56
 export EPOCH=4
 export MAX_SEQ_LENGTH=96
+export WARMUP_STEPS=1200
+export LR=2e-5
+# best performing funnel ws:1500
 
-# python3 run_hellaswag.py \
-                      # --model_name_or_path $MODEL_PATH\
-                      # --do_train \
-                      # --do_eval \
-                      # --num_train_epochs $EPOCH \
-                      # --output_dir $OUTPUT_PATH  \
-                      # --per_device_train_batch_size $BS \
-                      # --per_device_eval_batch_size $((BS*4)) \
-                      # --max_seq_length $MAX_SEQ_LENGTH \
-                      # --save_strategy epoch \
-                      # --evaluation_strategy epoch \
-                      # --warmup_steps 4000 \
-                      # --fp16 \
-                      # --overwrite_output_dir
 
-export MODEL_PATH='bert-large-uncased'
-export OUTPUT_PATH='/shared/mlrdir3/disk1/pxb190028/models/hellaswag/bert-large-uncased'
+export MODEL_PATH='/shared/mlrdir3/disk1/pxb190028/models/discosense/funnel-transformer-xlarge_ws_1500/'
+export OUTPUT_PATH='/shared/mlrdir3/disk1/pxb190028/models/seq_ft/funnel-transformer-xlarge_ws_1500/'
 python3 run_hellaswag.py \
                       --model_name_or_path $MODEL_PATH\
                       --do_train \
@@ -32,99 +19,8 @@ python3 run_hellaswag.py \
                       --max_seq_length $MAX_SEQ_LENGTH \
                       --save_strategy epoch \
                       --evaluation_strategy epoch \
-                      --warmup_steps 4000 \
+                      --warmup_steps $WARMUP_STEPS \
                       --fp16 \
                       --overwrite_output_dir
-
-# export MODEL_PATH='roberta-base'
-# export OUTPUT_PATH='/shared/mlrdir3/disk1/pxb190028/models/hellaswag/roberta-base'
-# python3 run_hellaswag.py \
-                      # --model_name_or_path $MODEL_PATH\
-                      # --do_train \
-                      # --do_eval \
-                      # --num_train_epochs $EPOCH \
-                      # --output_dir $OUTPUT_PATH  \
-                      # --per_device_train_batch_size $BS \
-                      # --per_device_eval_batch_size $((BS*4)) \
-                      # --max_seq_length $MAX_SEQ_LENGTH \
-                      # --save_strategy epoch \
-                      # --evaluation_strategy epoch \
-                      # --warmup_steps 4000 \
-                      # --fp16 \
-                      # --overwrite_output_dir
-
-# export MODEL_PATH='roberta-base'
-# export OUTPUT_PATH='/shared/mlrdir3/disk1/pxb190028/models/hellaswag/roberta-base'
-# python3 run_hellaswag.py \
-                      # --model_name_or_path $MODEL_PATH\
-                      # --do_train \
-                      # --do_eval \
-                      # --num_train_epochs $EPOCH \
-                      # --output_dir $OUTPUT_PATH  \
-                      # --per_device_train_batch_size $BS \
-                      # --per_device_eval_batch_size $((BS*4)) \
-                      # --max_seq_length $MAX_SEQ_LENGTH \
-                      # --save_strategy epoch \
-                      # --evaluation_strategy epoch \
-                      # --warmup_steps 4000 \
-                      # --fp16 \
-                      # --overwrite_output_dir
-
-export MODEL_PATH='allenai/longformer-base-4096'
-export OUTPUT_PATH='/shared/mlrdir3/disk1/pxb190028/models/hellaswag/longformer-base-4096'
-export BS=4
-python3 run_hellaswag.py \
-                      --model_name_or_path $MODEL_PATH\
-                      --do_train \
-                      --do_eval \
-                      --num_train_epochs $EPOCH \
-                      --output_dir $OUTPUT_PATH  \
-                      --per_device_train_batch_size $BS \
-                      --per_device_eval_batch_size $((BS*4)) \
-                      --max_seq_length $MAX_SEQ_LENGTH \
-                      --save_strategy epoch \
-                      --evaluation_strategy epoch \
-                      --warmup_steps 4000 \
-                      --fp16 \
-                      --overwrite_output_dir
-
-export MODEL_PATH='xlnet-large-cased'
-export OUTPUT_PATH='/shared/mlrdir3/disk1/pxb190028/models/hellaswag/xlnet-large-cased'
-export BS=128
-python3 run_hellaswag.py \
-                      --model_name_or_path $MODEL_PATH\
-                      --do_train \
-                      --do_eval \
-                      --num_train_epochs $EPOCH \
-                      --output_dir $OUTPUT_PATH  \
-                      --per_device_train_batch_size $BS \
-                      --per_device_eval_batch_size $((BS*4)) \
-                      --max_seq_length $MAX_SEQ_LENGTH \
-                      --save_strategy epoch \
-                      --evaluation_strategy epoch \
-                      --warmup_steps 4000 \
-                      --fp16 \
-                      --overwrite_output_dir
-
-
-
-# export MODEL_PATH='funnel-transformer/xlarge'
-# export OUTPUT_PATH='/shared/mlrdir3/disk1/pxb190028/models/hellaswag/funnel-transformer'
-# export BS=16
-# python3 run_hellaswag.py \
-                      # --model_name_or_path $MODEL_PATH\
-                      # --do_train \
-                      # --do_eval \
-                      # --num_train_epochs $EPOCH \
-                      # --output_dir $OUTPUT_PATH  \
-                      # --per_device_train_batch_size $BS \
-                      # --per_device_eval_batch_size $((BS*4)) \
-                      # --max_seq_length $MAX_SEQ_LENGTH \
-                      # --save_strategy epoch \
-                      # --evaluation_strategy epoch \
-                      # --warmup_steps 4000 \
-                      # --fp16 \
-                      # --overwrite_output_dir
-
 
 
